83d09153c9aa8076f88aff06c623a7f7a5ae87f0
ramkumarm15 <ramkumarmani2000@gmail.com>
2024-12-14 13:19:11 +0530

    change for access using ManagedIdentity

--- com.wrapper.centeralizedconfiguration/Program.cs
+++ com.wrapper.centeralizedconfiguration/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Core.Diagnostics;
 using Azure.Identity;
 using customlogging;
 
@@ -7,26 +8,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Logging.AddAzureWebAppDiagnostics();
-builder.AddLogging(builder.Configuration);
+//builder.AddLogging(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAzureAppConfiguration();
+
+using AzureEventSourceListener listener = AzureEventSourceListener.CreateConsoleLogger();
+
 builder.Configuration.AddAzureAppConfiguration(opt =>
 {
-    opt.Connect("Endpoint=https://pocappconfig.azconfig.io;Id=Inoy;Secret=ChbHpqtmee2JMjbMcXYw8liR0amYQp0GOngiRFA2ltInosqhkpPjJQQJ99AIACYeBjFPa7ZeAAACAZACVftz")
+    var cred = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
+    {
+        ManagedIdentityClientId = "37c94fdf-9c81-4f80-ab91-e9a16b459e6c"
+    });
+    opt.Connect(new Uri("https://pocappconfig.azconfig.io"), cred)
        .Select("Testing:*", null)
        .ConfigureRefresh(refOpt =>
        {
            refOpt.Register("Testing:Settings:Refresh", refreshAll: true);
-           refOpt.SetRefreshInterval(TimeSpan.FromSeconds(5));
+           refOpt.SetRefreshInterval(TimeSpan.FromSeconds(60));
        });
     builder.Services.AddSingleton(opt.GetRefresher());
 
     opt.ConfigureKeyVault(kv =>
     {
-        kv.SetCredential(new ClientSecretCredential("b845856f-adbb-4e37-9e00-9fed123512ee", "5da4786e-3f6a-4cb0-b75f-e9da632fd888", "1Tz8Q~IRL72NguBqE4xCnzBx-O9I2GaaPzsDFaDO"));
+        kv.SetCredential(cred);
     });
 });
 
